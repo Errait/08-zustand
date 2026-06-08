c107a651ae3b6e341d7ec7895a4e7ca90d5a5b8a
Anastasia Tkachova <oracleofmiracle@gmail.com>
2026-06-08 15:46:08 +0300

    feat: initialize project with Next.js and Zustand
    - Add package.json with dependencies and scripts for development
    - Create SVG assets for UI components
    - Add TypeScript configuration for strict type checking
    - Define types for notes including tags and interfaces

---
+++ app/notes/action/create/page.tsx
@@ -0,0 +1,39 @@
+import css from './CreateNote.module.css';
+import { Metadata } from 'next';
+import NoteForm from '@/components/NoteForm/NoteForm';
+
+export const metadata: Metadata = {
+  title: 'Create Note | NoteHub',
+  description: 'Creeate your personaly note',
+  openGraph: {
+    title: `Create Note | NoteHub`,
+    description: `Creeate your personaly note`,
+    url: `https://notehub.com/notes/aaction/create`,
+    type: 'website',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: `NoteHub Create Note`,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: `Create Note | NoteHub`,
+    description: `Creeate your personaly note`,
+    images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
+  },
+};
+
+export default async function CreateNote() {
+  return (
+    <main className={css.main}>
+      <div className={css.container}>
+        <h1 className={css.title}>Create note</h1>
+        <NoteForm />
+      </div>
+    </main>
+  );
+}
